c974ad28a29beb1fbeae58f53fc6376e7a40c7a9
IyanzDev <116157372+IyanzDev@users.noreply.github.com>
2022-10-21 15:28:41 +0700

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,13 +1,13 @@
 // Owner number
-global.owner = '6285807264974'
+global.owner = '6285742966427'
 // Owner name
-global.owner_name = 'Hanya Creator'
+global.owner_name = 'Ｉｙａｎ Ｄｅｖ＇ｓ'
 // Maximum upload file size limit (Default : 50 MB)
 global.max_upload = 50
 // Delay for spamming protection (Default : 3 seconds)
 global.cooldown = 3
 // User Limitation (Default : 25)
-global.limit = 25
+global.limit = 20
 // Time to be temporarily banned and others (Default : 30 minutes)
 global.timer = 1800000
 // Symbols that are excluded when adding a prefix (Don't change it)
@@ -15,7 +15,7 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '6285807264974@c.us'
+global.forwards = '6285742966427@c.us'
 // Get neoxr apikey by registering at https://api.neoxr.my.id
 global.Api = new (require('./neoxrApi'))(process.env.API_KEY)
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
@@ -24,11 +24,11 @@ global.chatai_key = 'MKPsfkgXLZPGrWoH'
 // Source version
 global.version = '5.4.7',
 // Footer text
-global.footer = 'Kanna bot'
+global.footer = 'Ozawa Bot'
 // Global status
 global.status = Object.freeze({
-   wait: Func.texted('bold', 'Processed . . .'),
-   invalid: Func.texted('bold', 'URL is Invalid!'),
+   wait: Func.texted('bold', 'Tunggu Bentar Kak...'),
+   invalid: Func.texted('bold', 'Urlnya Salah Kak...'),
    wrong: Func.texted('bold', 'Wrong format!'),
    getdata: Func.texted('bold', 'Scraping metadata . . .'),
    fail: Func.texted('bold', 'Can\'t get metadata!'),
